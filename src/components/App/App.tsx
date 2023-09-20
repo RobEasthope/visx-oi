037e135d9330aaf1daf91f0d957bebab47de6586
Rob Easthope <hello@robeasthope.com>
2023-09-20 12:58:16 +0100

    Test passing in data to LineChart

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,9 @@
 import { BarChart } from '../BarChart/BarChart';
 import { LineChart } from '../LineChart/LineChart';
+import { data } from '../LineChart/data';
 
 function App() {
-  return <BarChart />;
+  return <LineChart data={data} />;
 }
 
 export default App;
