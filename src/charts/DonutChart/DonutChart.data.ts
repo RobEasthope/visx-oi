e0ce5df93c81af25ad28825f357c76b0cf348033
Rob Easthope <hello@robeasthope.com>
2023-09-20 15:09:26 +0100

    Create DonutChartDataArray

--- src/charts/DonutChart/DonutChart.data.ts
+++ src/charts/DonutChart/DonutChart.data.ts
@@ -9,3 +9,38 @@ export const DonutChartData = {
   Mozilla: '0.12',
   'Other/Unknown': '0.01',
 };
+
+export const DonutChartDataArray = [
+  {
+    name: 'Google Chrome',
+    useage: 48.09,
+  },
+  {
+    name: 'Internet Explorer',
+    useage: 24.14,
+  },
+  {
+    name: 'Firefox',
+    useage: 18.82,
+  },
+  {
+    name: 'Safari',
+    useage: 7.46,
+  },
+  {
+    name: 'Microsoft Edge',
+    useage: 0.03,
+  },
+  {
+    name: 'Opera',
+    useage: 1.32,
+  },
+  {
+    name: 'Mozilla',
+    useage: 0.12,
+  },
+  {
+    name: 'Other/Unknown',
+    useage: 0.01,
+  },
+];
