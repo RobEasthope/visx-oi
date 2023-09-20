a4e8eaec82245f99b5d9bf39a594b8e128c23dd1
Rob Easthope <hello@robeasthope.com>
2023-09-20 14:33:29 +0100

    Add all chart to the app with some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { BarChart } from './charts/BarChart/BarChart';
 import { DonutChart } from './charts/DonutChart/DonutChart';
+import { LineChart } from './charts/LineChart/LineChart';
+import { LineChartData } from './charts/LineChart/LineChart.data';
 
 function App() {
-  return <DonutChart width={800} height={800} />;
+  return (
+    <main className="flex flex-col gap-16 p-8">
+      <h1 className="text-5xl font-bold">Chart examples</h1>
+      <div>
+        <h2 className="text-4xl font-bold mb-4">Bar chart</h2>
+        <BarChart />
+      </div>
+      <div>
+        <h2 className="text-4xl font-bold mb-4">Line chart</h2>
+        <LineChart data={LineChartData} />
+      </div>
+      <div>
+        <h2 className="text-4xl font-bold mb-4">Donut chart</h2>
+        <DonutChart width={800} height={800} />
+      </div>
+    </main>
+  );
 }
 
 export default App;
