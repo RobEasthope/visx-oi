db083f75b579a3a21c8aadcb4ba68582d1222c5d
Rob Easthope <hello@robeasthope.com>
2023-09-20 14:05:07 +0100

    Create DonutChart.data.ts

---
+++ src/charts/DonutChart/DonutChart.data.ts
@@ -0,0 +1,11 @@
+export const DonutChartData = {
+  date: '2015 Jun 15',
+  'Google Chrome': '48.09',
+  'Internet Explorer': '24.14',
+  Firefox: '18.82',
+  Safari: '7.46',
+  'Microsoft Edge': '0.03',
+  Opera: '1.32',
+  Mozilla: '0.12',
+  'Other/Unknown': '0.01',
+};
