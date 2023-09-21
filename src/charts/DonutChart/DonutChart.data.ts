3121cef0c8a8a5fbdf4e09e359ba8644a8eda647
Rob Easthope <hello@robeasthope.com>
2023-09-21 09:44:49 +0100

    Add colour data to array

--- src/charts/DonutChart/DonutChart.data.ts
+++ src/charts/DonutChart/DonutChart.data.ts
@@ -14,33 +14,41 @@ export const DonutChartDataArray = [
   {
     name: 'Google Chrome',
     useage: 48.09,
+    colour: 'rgba(255,255,255,0.7)',
   },
   {
     name: 'Internet Explorer',
     useage: 24.14,
+    colour: 'rgba(255,255,255,0.6)',
   },
   {
     name: 'Firefox',
     useage: 18.82,
+    colour: 'rgba(255,255,255,0.5)',
   },
   {
     name: 'Safari',
     useage: 7.46,
+    colour: 'rgba(255,255,255,0.4)',
   },
   {
     name: 'Microsoft Edge',
     useage: 0.03,
+    colour: 'rgba(255,255,255,0.3)',
   },
   {
     name: 'Opera',
     useage: 1.32,
+    colour: 'rgba(255,255,255,0.2)',
   },
   {
     name: 'Mozilla',
     useage: 0.12,
+    colour: 'rgba(255,255,255,0.1)',
   },
   {
     name: 'Other/Unknown',
     useage: 0.01,
+    colour: 'rgba(0,0,0,0.1)',
   },
 ];
